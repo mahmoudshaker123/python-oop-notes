62110b87f8feff5667491694caa9fc98881d2376
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2024-07-27 16:28:27 +0300

    last commit

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ for animal in animals:
 # Buddy says Woof!
 # Whiskers says Meow!
 
-
+#________________________________________________________
+# Encapsulation 
 class Person :
     def __init__(self , name , age ):
         self._name = name 
@@ -39,4 +40,27 @@ class Person :
 person = Person("MaHmoud shaker " , 24 )
 
 print(person.get_info())
+
+
+#_________________________________________
+# Abstraction 
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+# إنشاء كائن من الفئة Rectangle
+rectangle = Rectangle(5, 10)
+print(rectangle.area())  # يطبع: 50
 
